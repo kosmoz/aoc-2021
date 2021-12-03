4d6d8ce0397df0e3f9a7060986e6ad5e9ae2e460
Jakob Steiner <jakob.steiner@kosmoz.at>
2021-12-03 21:05:45 +0100

    day 2 part 2

--- day2/src/submarine.rs
+++ day2/src/submarine.rs
@@ -3,6 +3,7 @@ use crate::operation::Operation;
 pub struct Submarine {
     horizontal: i64,
     depth: i64,
+    aim: i64,
 }
 
 impl Submarine {
@@ -14,23 +15,31 @@ impl Submarine {
         Submarine {
             horizontal: 0,
             depth: 0,
+            aim: 0,
+        }
+    }
+
+    fn adjust_aim(self, by: i64) -> Submarine {
+        Submarine {
+            depth: self.depth,
+            horizontal: self.horizontal,
+            aim: self.aim + by,
+        }
+    }
+
+    fn move_forward(self, by: i64) -> Submarine {
+        Submarine {
+            horizontal: self.horizontal + by,
+            depth: self.depth + by * self.aim,
+            aim: self.aim,
         }
     }
 
     pub fn apply(self, op: Operation) -> Submarine {
         match op {
-            Operation::Up(a) => Submarine {
-                depth: self.depth - a,
-                horizontal: self.horizontal,
-            },
-            Operation::Down(a) => Submarine {
-                depth: self.depth + a,
-                horizontal: self.horizontal,
-            },
-            Operation::Forward(a) => Submarine {
-                depth: self.depth,
-                horizontal: self.horizontal + a,
-            },
+            Operation::Up(a) => self.adjust_aim(-a),
+            Operation::Down(a) => self.adjust_aim(a),
+            Operation::Forward(a) => self.move_forward(a),
         }
     }
 }
